5f6965d806b0b0097f338bd77c844a3e5555ff3f
Marina Savelyeva <mar.saveleva.18@gmail.com>
2024-04-30 20:53:02 +0300

    1 number

--- task_1/decoded.js
+++ task_1/decoded.js
@@ -1,33 +1,23 @@
 export function decodeFields(encodedData, translations) {
   const decodedData = [];
-  const uniqueIds = new Set();
+  const untranslatedValuesSet = new Set();
 
   encodedData.forEach(item => {
-    const decodedItem = { ...item }; 
+    const decodedItem = { ...item };
 
     for (const key in item) {
       if (key.endsWith('Id') && !['groupId', 'service', 'formatSize', 'ca'].includes(key)) {
-        const translationKey = item[key];
-        if (translations[translationKey]) {
+        let translationKey = item[key];
+        if (translations[translationKey] !== undefined) {
           decodedItem[key] = translations[translationKey];
+        } else {
+          untranslatedValuesSet.add(translationKey);
         }
-        uniqueIds.add(translationKey);
       }
     }
-
     decodedData.push(decodedItem);
   });
 
-  return { decodedData, uniqueIds };
+  const missingValues = Array.from(untranslatedValuesSet);//преобразуем в массив
+  return { decodedData, missingValues };
 }
-
-export function findMissingValues(uniqueIds, translations) {
-  let missingValues = [];
-  uniqueIds.forEach(id => {
-      if (translations[id] === undefined) {
-          missingValues.push(id);
-      }
-  });
-  return missingValues;
-}
-
--- task_1/index.js
+++ task_1/index.js
@@ -1,18 +1,14 @@
 import {encoded, translations} from './data.js'
-import { decodeFields, findMissingValues } from './decoded.js';
+import { decodeFields } from './decoded.js';
 
 console.log("Let's rock")
 console.log(encoded, translations)
 
-
-const { decodedData, uniqueIds } = decodeFields(encoded, translations);
+const { decodedData, missingValues } = decodeFields(encoded, translations);
 
 console.log("Декодировка:");
 console.log(decodedData);
 
-console.log("\nId которые встречаются в encoded:");
-console.log(Array.from(uniqueIds));
 
 console.log("\nId которые встречаются только в encoded, но в translations их нет:");
-const missingValues = findMissingValues(uniqueIds, translations);
 console.log(missingValues);
